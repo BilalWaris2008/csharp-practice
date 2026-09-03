489500da3124ae98950b35eac0d41952384abb64
M.Bilal Waris <bilalwaris543@gmail.com>
2026-09-04 00:17:01 +0500

    Added Simple Calculator

---
+++ SimpleCalculator.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("\n========= SIMPLE CALCULATOR =========\n");
+
+Console.Write("Enter First Number: ");
+double firstnum = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Second Number: ");
+double secondnum = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"\n{firstnum} + {secondnum} = {firstnum + secondnum}");
+Console.WriteLine($"{firstnum} - {secondnum} = {firstnum - secondnum}");
+Console.WriteLine($"{firstnum} * {secondnum} = {firstnum * secondnum}");
+Console.WriteLine($"{firstnum} / {secondnum} = {firstnum / secondnum}");
+
+Console.WriteLine("\n======================================");
